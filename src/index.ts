50b89d581e70b2a9a8b08f4b2c55f08faa76e286
Timothy Curchod <timofeyc@hotmail.com>
2018-10-08 23:49:54 +1100

    Moved the footnote function into index

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,13 @@ export * from './models/anti-pattern';
 export * from './components/notification.component';
 export * from './providers/wikidata';
 export * from './providers/convenience';
+exports.removeFootnotes = function(description: string) {
+    if (description) {
+        const indexOfBracket = description.indexOf('[');
+        if (indexOfBracket !== -1) {
+            return description.substring(0, indexOfBracket);
+        } else {
+            return description;
+        }
+    }
+}
