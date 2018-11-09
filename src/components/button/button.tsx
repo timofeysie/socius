595fe0b794396b735f53e685b43202115d03a743
Timothy Curchod <timofeyc@hotmail.com>
2018-11-10 08:42:57 +1100

    Added notes on the button css class

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,6 +1,22 @@
 import { Component, Prop } from '@stencil/core';
 import { CssClassMap } from '../../utils/interfaces';
 
+/**
+ * Our props color, shape, and size.
+ * Instead of defining my types for these props to be of type string, 
+ * We're going a step further and defining that each prop can be one of several specific strings. 
+ * This allows for much more effective hints and dot complete a.k.a. IntelliSense.
+ * There are a few specific sizes, colors, shapes, and types that are implemented. 
+ * On top of that, we initialize those props to some preferred defaults, 
+ * so that if you simply create a button component with no defined props, 
+ * it'll look like a default button.
+ * 
+ * We use those props to build a map of CSS classes to apply to our components. 
+ * The CssClassMap defines an interface for this purpose. 
+ * The class map type is a map of a key, the class name, and the value being a boolean. 
+ * This specifies whether we want the classes to be applied or not. 
+ * 
+ */
 @Component({
   tag: 'curchod-button',
   styleUrl: 'button.scss',
@@ -22,6 +38,7 @@ export class Button {
   }
   render() {
     const classMap = this.getCssClassMap();
+    console.log('classMap',classMap);
     return (
       <button type={this.type} 
         class={classMap} 
--- www/build/curchod-ui.js
+++ www/build/curchod-ui.js
@@ -1,6 +1,6 @@
 /*!
  * Built with http://stenciljs.com
- * 2018-11-09T20:54:38
+ * 2018-11-09T21:36:54
  */
 (function(win, doc, namespace, fsNamespace, resourcesUrl, appCore, appCoreSsr, appCorePolyfilled, hydratedCssClass, components) {
 
