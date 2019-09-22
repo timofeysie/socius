3854ef4d0b02ce8b28689e36475427e10a2d3880
Timothy Curchod <timofeyc@hotmail.com>
2019-09-22 10:34:45 +1000

    #1 #2 after merge with development branch

--- www/build/folia-ui.js
+++ www/build/folia-ui.js
@@ -1,6 +1,6 @@
 /*!
  * Built with http://stenciljs.com
- * 2019-03-30T13:43:33
+ * 2019-09-22T00:03:25
  */
 (function(win, doc, namespace, fsNamespace, resourcesUrl, appCore, appCoreSsr, appCorePolyfilled, hydratedCssClass, components) {
 
